5f77181e76a52825a3292dab45cfc1ba27d79d0e
Leagueathyst <league@WTS5.local>
2017-07-16 17:30:06 -0700

    day 1

---
+++ src/day1/ForLoopGauntlet.java
@@ -0,0 +1,41 @@
+package day1;
+
+public class ForLoopGauntlet {
+	public static void main(String[] args) {
+		challenge2();
+		
+	
+	
+	
+	
+	
+	}
+	public static void challenge1(){
+  for (int i = 0; i < 101; i++) {
+	System.out.print(i);
+}
+	}
+	
+	public static void challenge2(){
+		  for (int i = 100; i > -1; i--) {
+			System.out.print(i);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		  }
+	
+	
+		 }
+	
+	
+}
+
+
+
