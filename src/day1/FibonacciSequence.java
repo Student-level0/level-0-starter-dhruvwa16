f2a25b4689e320ee25b97d4c925ddb953e8a1515
dhruvwa16 <league@WorthyMini-5.attlocal.net>
2017-07-23 17:27:42 -0700

    i

---
+++ src/day1/FibonacciSequence.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class FibonacciSequence {
+public static void main(String[] args) {
+	
+	int a = 1;
+	int 
+	for (int i = 0; i < args.length; i += a) {
+		System.out.print(i a);
+		
+	}
+	
+	
+	
+	
+	
+	
+}
+}
--- src/day1/ForLoopGauntlet.java
+++ src/day1/ForLoopGauntlet.java
@@ -2,7 +2,7 @@ package day1;
 
 public class ForLoopGauntlet {
 	public static void main(String[] args) {
-		challenge2();
+		challenge8();
 		
 	
 	
@@ -12,30 +12,111 @@ public class ForLoopGauntlet {
 	}
 	public static void challenge1(){
   for (int i = 0; i < 101; i++) {
-	System.out.print(i);
+	System.out.println(i);
 }
 	}
 	
 	public static void challenge2(){
 		  for (int i = 100; i > -1; i--) {
-			System.out.print(i);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+			System.out.println(i);
 		  }
+			
+	}  
+		  
+		  public static void challenge3(){
+				  for (int i =2; i < 101; i+=2) {
+					System.out.println(i);
+			
+				  }
+		  }
+					 public static void challenge4(){
+						  for (int i =1; i < 100; i+=2) {
+							System.out.println(i);
+					
+						  }
+					 }
+					
+					public static void challenge5(){
+						 for (int i =1; i < 500; i++) {
+						 System.out.println(i);
+					if (i % 2 == 0){
+					 System.out.print("even");
+			}	 
+					
+					else{
+						System.out.print("odd");
+					 
+						 
+					}
+					
+					}
+						 }
+			
+					 public static void challenge6(){
+						  for (int i = 7; i < 701; i+=7) {
+							System.out.println(i);
+					
+						  
+					 }
+					 
+			
+			
+					 }
+					 
+						
+					 
+					
+					 public static void challenge7(){
+						for (int i = 0; i < 3; i++) {
+							
+							for (int j = 0; j < 3; j++) {
+							
+								System.out.println(i+" "+j);
+							}
+						}
+						
+						} 
+							
+					 public static void challenge8(){
+							for (int i = 1; i < 8; ) {
+								
+								for (int j = 0; j < 3; j++) {
+								
+									
+									System.out.println(i);
+								}
+							}
+							
+							} 
+						  
+					 
+					 
+			
+			
+					 }
+						  
+					 
+					 
+			
+			
+					 
+
 	
 	
-		 }
 	
 	
-}
+	
+	
+	
+					
+					
+
+	
+	
+		 
+	
+	
+
 
 
 
